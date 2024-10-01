c8341fd8b03b363b599979b672734f4813965dd3
Namrata Diware <namrata.diware@gmail.com>
2024-10-02 01:34:46 +0530

    added my feed section
    added my feed section and added routes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import MyFeed from './components/MyFeedSecion/MyFeed';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/myfeed" element={<MyFeed />} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
