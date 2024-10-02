7dfb4dbe97830f80459c517fadc29176dc481992
prathamesh-pichkate <prathameshpichkate@gmail.com>
2024-10-02 10:52:00 +0530

    Homepage Redesign

--- frontend/src/components/Herosection/Herosection.jsx
+++ frontend/src/components/Herosection/Herosection.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import "./Herosection.css";
-// import heroimg from '../../assets/hero-img.png';
 
 function Herosection() {
   return (
     <div className="hero-section">
       <h1 className="highlighted-heading">
-        <span class="white-text">INDIA’S FIRST</span>
+        <span className="white-text">INDIA’S FIRST</span>
         <br />
         <span className="white-text">SMART </span>
         <span className="highlighted-text">News FIN-APP</span>
       </h1>
- 
+
       <p className="hero-description">“Where Intelligence Meets Finance”</p>
-      <button className="hero-button">WWW.FinNews.IN</button>
-      {/* <img className="heroimg" src={heroimg} alt="" /> */}
+      <button className="hero-button">www.FinNews.in</button>
+
       <div className="scroll-down">
         <p>Scroll down for more</p>
         <div className="arrow"></div>
       </div>
-
     </div>
   );
 }
