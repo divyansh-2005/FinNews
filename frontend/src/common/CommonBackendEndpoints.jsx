2365442b8dc512cc9085a5d436b31f7b1523e51a
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-10-04 02:18:37 +0530

    deploy backend

--- frontend/src/common/CommonBackendEndpoints.jsx
+++ frontend/src/common/CommonBackendEndpoints.jsx
@@ -1,4 +1,6 @@
-const backendDomain = 'http://localhost:5000';
+const dotenv = require("dotenv");
+dotenv.config(); 
+const backendDomain = process.env.BACKEND_DOMAIN || 'http://localhost:5000';
 
 const commonendpoint = {
     latestFinancialNews : {
