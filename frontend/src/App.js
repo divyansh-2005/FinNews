79f4839a53d444ecfea989d3bed4e52b7a0787f3
prathamesh-pichkate <prathameshpichkate@gmail.com>
2024-10-04 22:25:49 +0530

    redesign the government scheme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import MyFeed from './components/MyFeedSecion/MyFeed';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import BlogSection from './components/Blogs/BlogSection';
+import React from "react";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MyFeed from "./components/MyFeedSecion/MyFeed";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import BlogSection from "./components/Blogs/BlogSection";
 import BlogPageTemplate from "./components/Blogs/BlogPageTemplate";
 
 function App() {
   return (
-  
-        <HelmetProvider>
-          <Router>
-            <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/myfeed" element={<MyFeed />} />
-              <Route path="/blog/:key" element={<BlogPageTemplate />} />
-              <Route path="/blogs" element={<BlogSection />} />
-            </Routes>
-            <Footer />
-          </Router>
-        </HelmetProvider>  // Closing tag for HelmetProvider
-      );
-    }
-    
- 
+    <HelmetProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/myfeed" element={<MyFeed />} />
+          <Route path="/blog/:key" element={<BlogPageTemplate />} />
+          <Route path="/blogs" element={<BlogSection />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </HelmetProvider>
+  );
+}
+
 export default App;
--- frontend/src/components/Daily-NewsLetter/DailyNews.jsx
+++ frontend/src/components/Daily-NewsLetter/DailyNews.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import SectionHeading from '../SectionHeading/SectionHeading';
-import styles from './DailyNews.module.css'; // Import the CSS Module
-import backendEndPoint from '../../common/CommonBackendEndpoints.jsx'
-import { useEffect, useState } from 'react';
+import React from "react";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import styles from "./DailyNews.module.css"; // Import the CSS Module
+import backendEndPoint from "../../common/CommonBackendEndpoints.jsx";
+import { useEffect, useState } from "react";
 function DailyNews() {
-const [dailyNewsLetter , setdailyNewsLetter] = useState([])
+  const [dailyNewsLetter, setdailyNewsLetter] = useState([]);
 
-console.log('dailyNewsLetter :', dailyNewsLetter);
+  console.log("dailyNewsLetter :", dailyNewsLetter);
 
-const fetchNewsLetter = async (req ,res) => {
-  try {
-    const response = await fetch(backendEndPoint.dailyNewsetter.url);
-    if(!response.ok) {
-      throw new Error('Network response was not ok');
+  const fetchNewsLetter = async (req, res) => {
+    try {
+      const response = await fetch(backendEndPoint.dailyNewsetter.url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const responseData = await response.json();
+      console.log("data:", response);
+      setdailyNewsLetter(responseData);
+    } catch (error) {
+      console.log("error :", error);
     }
-    const responseData = await response.json();
-    console.log('data:', response)
-    setdailyNewsLetter(responseData)
-  } catch (error) {
-    console.log('error :', error)
-  }
-}
+  };
 
-useEffect(()=>{
-  fetchNewsLetter();
-},[])
+  useEffect(() => {
+    fetchNewsLetter();
+  }, []);
   return (
     <>
       <SectionHeading title="Daily NewsLetter" />
@@ -34,27 +34,36 @@ useEffect(()=>{
             <div className={styles.textContent}>
               <div className={styles.companyTime}>
                 <span className={styles.companyName}>
-                {news.title.includes("OpenAI") ? "OpenAI" :
-                  news.title.includes("Swiggy") ? "Swiggy" :
-                  news.title.includes("SpiceJet") ? "SpiceJet" :
-                  news.title.includes("Apple") ? "Apple" :
-                  news.title.includes("Zerodha") ? "Zerodha" :
-                  news.title.includes("Zepto") ? "Zepto" :
-                  news.title.includes("Gemini") ? "Gemini" :
-                  news.title.includes("NASA") ? "NASA" :
-                  news.title.includes("Nasa") ? "Nasa" :
-                  news.title.includes("Samsung") ? "Samsung" : "Unknown"}
+                  {news.title.includes("OpenAI")
+                    ? "OpenAI"
+                    : news.title.includes("Swiggy")
+                    ? "Swiggy"
+                    : news.title.includes("SpiceJet")
+                    ? "SpiceJet"
+                    : news.title.includes("Apple")
+                    ? "Apple"
+                    : news.title.includes("Zerodha")
+                    ? "Zerodha"
+                    : news.title.includes("Zepto")
+                    ? "Zepto"
+                    : news.title.includes("Gemini")
+                    ? "Gemini"
+                    : news.title.includes("NASA")
+                    ? "NASA"
+                    : news.title.includes("Nasa")
+                    ? "Nasa"
+                    : news.title.includes("Samsung")
+                    ? "Samsung"
+                    : "Unknown"}
                 </span>
                 <span className={styles.time}>{news.publishedAt} </span>
               </div>
-              <h1 className={styles.headingNews}>
-               {news.title}
-              </h1>
-              <div className={styles.description}>
-               {news.description}
-              </div>
+              <h1 className={styles.headingNews}>{news.title}</h1>
+              <div className={styles.description}>{news.description}</div>
             </div>
-            <div className={styles.image}><img src={news.image} alt='containt-img' /></div>
+            <div className={styles.image}>
+              <img src={news.image} alt="containt-img" />
+            </div>
           </div>
         ))}
       </div>
