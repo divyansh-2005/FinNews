80e9c841eb5ff35fd9cfed9abf158287f9ff51a9
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2024-10-12 19:29:45 +0530

    added contact us page

--- frontend/src/components/ContactUsPage/ContactUs.jsx
+++ frontend/src/components/ContactUsPage/ContactUs.jsx
@@ -4,7 +4,7 @@ import "./ContactUs.css";
 function ContactUs() {
   return (
     <div className="contact-us">
-      <h2 className="contact-heading">Get in Touch with Us</h2>
+      <h2 className="contact-heading"> Get in Touch with Us </h2>
       <form className="contact-form">
         <label htmlFor="name" className="contact-label">Name</label>
         <input type="text" id="name" className="contact-input" placeholder="Enter your name" />
